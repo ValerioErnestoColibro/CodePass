6e1fd8bfb1edb2c374b7c3c519c99a299ea283cb
Mx444 <mybbtester@gmail.com>
2024-05-16 20:54:09 +0200

    chore(config): config npm packages cryptojs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,18 +2,27 @@
 
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import nodePlugin from 'eslint-plugin-node';
 
 export default [
   {
     files: ['src/**/*.js'],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 2021,
+      sourceType: 'script',
+    },
+    plugins: {
+      node: nodePlugin,
     },
     rules: {
       'no-unused-vars': 'warn',
-      'no-undef': 'warn',
+      'no-undef': 'off',
+      'node/no-missing-import': 'error',
     },
-    ignores: ['node_modules', '**/*.config.js', '!**/eslint.config.js'],
+    ignores: ['node_modules', '**/*.config.js', '!**/eslint.config.mjs'],
   },
   pluginJs.configs.recommended,
 ];
