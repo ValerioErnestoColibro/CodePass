2ecdeb8b355649672a44a6396e9043a3e1821ad3
Mx444 <mybbtester@gmail.com>
2024-05-22 22:48:34 +0200

    feat(website): modifiche User , UserManager & PasswordItems

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,15 +3,56 @@
 'use strict';
 
 const PasswordItems = require('./PasswordItems.test');
+const UsersManager = require('../services/UsersManager.test');
 const { v4: uuidv4 } = require('uuid');
+const validator = require('validator');
 class User {
+  #username;
+  #email;
+  #password;
+  #master;
+  #passwordItems;
+  #id;
   constructor(username, email, password, master) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-    this.master = master;
-    this.passwordItems = [];
-    this.id = uuidv4();
+    this.#username = username;
+    this.#email = email;
+    this.#password = password;
+    this.#master = master;
+    this.#passwordItems = [];
+    this.#id = uuidv4();
+  }
+  getUsername() {
+    return this.#username;
+  }
+  setUsername(username) {
+    this.#username = username;
+  }
+  getEmail() {
+    return this.#email;
+  }
+  setEmail(email) {
+    this.#email = email;
+  }
+  getPassword() {
+    return this.#password;
+  }
+  setPassword(password) {
+    this.#password = password;
+  }
+  getMaster() {
+    return this.#master;
+  }
+  setMaster(master) {
+    this.#master = master;
+  }
+  getPasswordItems() {
+    return this.#passwordItems;
+  }
+  addPasswordItem(passwordItem) {
+    this.#passwordItems.push(passwordItem);
+  }
+  getID() {
+    return this.#id;
   }
 }
 
--- test/services/UsersManager.test.js
+++ test/services/UsersManager.test.js
@@ -3,52 +3,210 @@
 'use strict';
 
 const User = require('../models/User.test');
-//validator
-// cryptojs or bcrypt
+var CryptoJS = require('crypto-js');
+const validator = require('validator');
+
+function validateUserName(username) {
+  return /^[a-z0-9_]{3,16}$/.test(username);
+}
+function validatePassword(password) {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/.test(password);
+}
+
 class UsersManager {
+  #users;
+  #session;
   constructor() {
-    this.users = [];
+    this.#users = [];
+    this.#session = null;
+  }
+  #hashWithKey(text, key) {
+    return CryptoJS.HmacSHA256(text, key).toString(CryptoJS.enc.Hex);
   }
   signup(username, email, password, master) {
-    // da implementare
-    const user = new User(username, email, password, master);
-    this.users.push(user);
-  }
-  login(username, password) {
-    // da implementare
-    const user = this.users.find(u => u.username === username && u.password === password);
-    return user || console.log('');
-  }
-  deleteUser(email) {
-    //da cambiare ID impl
-    const index = this.users.findIndex(e => e.email === email);
-    if (index != -1) {
-      this.users.splice(index, 1);
+    if (!this.#session) {
+      if (this.#users.some(u => u.getUsername() === username)) {
+        console.log('Username già in uso! ❌');
+        return null;
+      }
+      if (!validateUserName(username)) {
+        console.log('Username non valido! ❌');
+        return null;
+      }
+      if (!validator.isEmail(email)) {
+        console.log('Email non valida! ❌');
+        return null;
+      }
+      if (!validatePassword(password)) {
+        console.log('Password non valida! ❌');
+        return null;
+      }
+      if (!validatePassword(master)) {
+        console.log('Master Password non valida! ❌');
+        return null;
+      }
+      const hashedPassword = this.#hashWithKey(password, master);
+      const user = new User(username, email, hashedPassword, master);
+      user.setUsername(username);
+      user.setEmail(email);
+      user.setPassword(hashedPassword);
+      user.setMaster(master);
+      this.#users.push(user);
+      console.log(`Utente ${username} registrato! 👤`);
+      return user;
     } else {
-      console.log('Utente non trovato!');
+      console.log(`Sei giá loggato ! 👤`);
     }
   }
-  updateEmail(username, password, newEmail) {
-    // da implementare
-    const user = this.users.find(u => u.username === username && u.password === password);
-    if (!user) {
-      console.log('Utente non trovato!');
+
+  login(username, password, master) {
+    if (!this.#session) {
+      const hashedPassword = this.#hashWithKey(password, master);
+      const user = this.#users.find(
+        u => u.getUsername() === username && u.getPassword() === hashedPassword && u.getMaster() === master,
+      );
+      if (!!user) {
+        this.#session = { id: user.getID(), username: user.getUsername() };
+        console.log(`Utente ${username} loggato! ✅`);
+        return user;
+      } else {
+        console.log(`Utente non trovato! 🔎`);
+        return null;
+      }
     } else {
-      user.email = newEmail;
+      console.log(`Sei giá loggato ! ✅`);
+      this.getSession();
     }
   }
-  updatePassword(username, password, newPassword) {
-    // da implementare
-    const user = this.users.find(u => u.username === username && u.password === password);
-    if (!user) {
-      console.log('Utente non trovato!');
+  logout() {
+    if (!!this.#session) {
+      const username = this.#session.username;
+      this.#session = null;
+      console.log(`Utente ${username} disconnesso! ✅`);
+      return true;
     } else {
-      user.password = newPassword;
+      console.log(`Prima devi effettuare l'accesso ! 👤`);
+      return false;
+    }
+  }
+  deleteUser(username, password, master) {
+    if (!!this.#session) {
+      const hashedPassword = this.#hashWithKey(password, master);
+      const index = this.#users.findIndex(
+        u => u.getUsername() === username && u.getPassword() === hashedPassword && u.getMaster() === master,
+      );
+      if (index !== -1) {
+        this.#users.splice(index, 1);
+        this.#session = { id: null, username: null };
+        console.log(`Utente ${username} eliminato! ✅`);
+      } else {
+        console.log(`Utente ${username} non trovato! 🔎`);
+      }
+    } else {
+      console.log(`Prima devi effettuare l'accesso ! 👤`);
+    }
+  }
+  updateUsername(username, password, master, newUsername) {
+    if (!!this.#session) {
+      const hashedPassword = this.#hashWithKey(password, master);
+      const user = this.#users.find(
+        u => u.getUsername() === username && u.getPassword() === hashedPassword && u.getMaster() === master,
+      );
+      if (!!user) {
+        if (validateUserName(newUsername)) {
+          user.setUsername(newUsername);
+          this.#session = { id: user.getID(), username: user.getUsername() };
+          console.log(`Username dell'utente ${username} aggiornato! ✅`);
+        } else {
+          console.log(`Username ${newUsername} non valido! ❌`);
+        }
+      } else {
+        console.log(`Utente ${username} non trovato! 🔎`);
+      }
+    } else {
+      console.log(`Prima devi effettuare l'accesso ! 👤`);
+    }
+  }
+  updateEmail(username, password, master, newEmail) {
+    if (!!this.#session) {
+      const hashedPassword = this.#hashWithKey(password, master);
+      const user = this.#users.find(
+        u => u.getUsername() === username && u.getPassword() === hashedPassword && u.getMaster() === master,
+      );
+      if (!!user) {
+        if (validator.isEmail(newEmail)) {
+          user.setEmail(newEmail);
+          console.log(`Email dell'utente ${username} aggiornata! ✅`);
+        } else {
+          console.log(`Email ${newEmail} non valida! ❌`);
+        }
+      } else {
+        console.log(`Utente ${username} non trovato! 🔎`);
+      }
+    } else {
+      console.log(`Prima devi effettuare l'accesso ! 👤`);
+    }
+  }
+  updatePassword(username, password, master, newPassword) {
+    if (!!this.#session) {
+      const hashedPassword = this.#hashWithKey(password, master);
+      const user = this.#users.find(
+        u => u.getUsername() === username && u.getPassword() === hashedPassword && u.getMaster() === master,
+      );
+      if (user) {
+        if (validatePassword(newPassword)) {
+          user.setPassword(newPassword);
+          console.log(`Password dell'utente ${username} aggiornata! ✅`);
+        } else {
+          console.log(`Password ${newPassword} non valida! ❌`);
+        }
+      } else {
+        console.log(`Utente ${username} non trovato! 🔎`);
+      }
+    } else {
+      console.log(`Prima devi effettuare l'accesso ! 👤`);
+    }
+  }
+  updateMasterPassword(username, password, master, newMaster) {
+    if (!!this.#session) {
+      const hashedPassword = this.#hashWithKey(password, master);
+      const user = this.#users.find(
+        u => u.getUsername() === username && u.getPassword() === hashedPassword && u.getMaster() === master,
+      );
+      if (user) {
+        if (validatePassword(newMaster)) {
+          user.setMaster(newMaster);
+          console.log(`Master Password dell'utente ${username} aggiornata! ✅`);
+        } else {
+          console.log(`Master Password ${newMaster} non valida! ❌`);
+        }
+      } else {
+        console.log(`Utente ${username} non trovato! 🔎`);
+      }
+    } else {
+      console.log(`Prima devi effettuare l'accesso ! 👤`);
+    }
+  }
+  // func test
+  showUser(username) {
+    const user = this.#users.find(u => u.getUsername() === username);
+    if (user) {
+      console.log(
+        `👤 Username: ${user.getUsername()}, ✉️ Email: ${user.getEmail()}, 🔒 Password: ${user.getPassword()}, 🔐 MasterPassword: ${user.getMaster()}, 🆔 ID : ${user.getID()},📦 Items : ${user.getPasswordItems()}`,
+      );
+    } else {
+      console.log('Utente non trovato! ❌');
     }
   }
   listUser() {
-    // test update email & pass
-    this.users.forEach(x => console.log(`${x.email},${x.username},${x.password}`));
+    this.#users.forEach(user =>
+      console.log(
+        `👤 Username: ${user.getUsername()}, ✉️ Email: ${user.getEmail()}, 🔒 Password: ${user.getPassword()}, 🔐 MasterPassword: ${user.getMaster()}, 🆔 ID : ${user.getID()},📦 Items : ${user.getPasswordItems()}`,
+      ),
+    );
+  }
+  getSession() {
+    return console.log(this.#session);
   }
 }
 
