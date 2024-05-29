fbcf943ccf261442a3e4d887ad8a08d588cdcfe0
Mx444 <mybbtester@gmail.com>
2024-05-29 09:24:08 +0200

    fix(website): clean code usermanager

--- test/App.test.js
+++ test/App.test.js
@@ -12,33 +12,18 @@ const app = new CodePass();
 //                                                                                                 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 */
-console.log('Session 🌐');
+
+//check param , validate , duplicate & session
 app.users.getSession();
-console.log('SIGNUP & LOGIN 👤');
-const firstUserSign = app.users.signup('mariorossi', 'mariorossi@gmail.com', 'Password1234@!', 'Master5678@!');
-const firstUserLoging = app.users.login('mariorossi', 'Password1234@!', 'Master5678@!');
-console.log('SESSION 🌐 & LIST 👤');
+const $persona1 = app.users.signup('mariorossi', 'mariorossi@gmail.com', 'Password1234@!', 'Master1234@!');
+app.users.login('mariorossi', 'Password1234@!', 'Master1234@!');
+// app.users.login('mariorossi', 'Password1234@!', 'Master1234@!');
 app.users.getSession();
-app.users.listUser();
-// console.log('LOGOUT ❌ & SESSION 🌐');
 // app.users.logout();
 // app.users.getSession();
-// console.log('DELETE ❌');
-// app.users.deleteUser('mariorossi', 'Password1234@!', 'Master5678@!');
-// app.users.listUser();
-// console.log('USER 2 👤');
-// console.log('SIGNUP & LOGIN 👤');
-// const secondUserSign = app.users.signup('luciaverdi', 'luciaverdi@proton.me', 'Password1234@!', 'Master5678@!');
-// const secondUserLogin = app.users.login('luciaverdi', 'Password1234@!', 'Master5678@!');
-// console.log('SESSION 🌐 & LIST 👤');
-// app.users.getSession();
-// app.users.listUser();
-// app.users.getarray();
-// console.log('LOGOUT ❌ & SESSION 🌐');
 // app.users.logout();
-// app.users.getSession();
-// console.log('DELETE ❌');
-// app.users.deleteUser('luciaverdi', 'Password1234@!', 'Master5678@!');
+// app.users.listUser();
+// app.users.deleteUser('mariorossi', 'Password1234@!', 'Master1234@!');
 // app.users.listUser();
 
 /*
@@ -49,20 +34,12 @@ app.users.listUser();
 //////////////////////////////////////////////////////////////////////////////////////////////////
 */
 
-// console.log('UPDATE: USERNAME,EMAIL,PASSWORD & MASTER ✅');
-// app.users.updateEmail('luciaverdi', 'Password1234@!', 'Master5678@!', 'newlucia@gmail.com');
-// app.users.updateUsername('luciaverdi', 'Password1234@!', 'Master5678@!', 'newluciauser');
-// app.users.updatePassword('newluciauser', 'Password1234@!', 'Master5678@!', 'newPass123@!');
-// app.users.updateMaster('newluciauser', 'newPass123@!', 'Master5678@!', 'newMast1234@!');
-// app.users.listUser();
-// app.users.logout();
-
-console.log('UTENTE 1 👤');
-app.users.login('mariorossi', 'Password1234@!', 'Master5678@!');
-app.users.updateUsername('mariorossi', 'Password1234@!', 'Master5678@!', 'newrossi');
-app.users.updateEmail('newrossi', 'Password1234@!', 'Master5678@!', 'newrossi@outlook.it');
-app.users.updatePassword('newrossi', 'Password1234@!', 'Master5678@!', 'newPass123@!');
-const fistUserUpdated = app.users.updateMaster('newrossi', 'newPass123@!', 'Master5678@!', 'newMast1234@!');
+//check param , validate , duplicate & session
+app.users.updateUsername('mariorossi', 'Password1234@!', 'Master1234@!', 'newmariorossi');
+app.users.getSession(); // check update session
+app.users.updateEmail('newmariorossi', 'Password1234@!', 'Master1234@!', 'newmariorossi@gmail.com');
+app.users.updatePassword('newmariorossi', 'Password1234@!', 'Master1234@!', 'Newpa123@!');
+app.users.updateMaster('newmariorossi', 'Newpa123@!', 'Master1234@!', 'Newmaster123@!');
 app.users.listUser();
 
 /*
@@ -72,17 +49,6 @@ app.users.listUser();
 //                                                                                               //
 //////////////////////////////////////////////////////////////////////////////////////////////////
 */
-console.log('TEST PASSWORDITEMS 📦');
-app.password.addPasswordItem(fistUserUpdated, 'email', 'passwrd', 'website', 'description');
-app.password.addPasswordItem(fistUserUpdated, '1', '2', '3', '4');
-app.password.updateEmailItem(fistUserUpdated, 'email', 'newemail');
-app.password.updatePasswordItem(fistUserUpdated, 'newemail', 'newPASSWORD');
-app.password.updateWebSiteItem(fistUserUpdated, 'newemail', 'newWEBSITE');
-app.password.updateDescriptionItem(fistUserUpdated, 'newemail', 'newDESCRIPTION');
-app.password.updateDescriptionItem(fistUserUpdated, '1', 'newDESCRIPTION0000');
-// app.password.deletePasswordItem(fistUserUpdated, 'newemail');
-app.users.listUser();
-fistUserUpdated.print();
 
 /*
 ////////////////////////////////////////////////////////////////////////////////////////////////////
